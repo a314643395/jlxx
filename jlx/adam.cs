15bc2e79142f5f56738d683bd5eb2b1e09980d13
a314643395 <314643395@qq.com>
2017-01-05 15:12:04 +0800

    2017 1 5

---
+++ jlx/adam.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace jlx
+{
+    /*
+     Copyright © 2015，Adam，All Rights Reserved.
+     * Copyright ownership belongs to JLX,shall not be reproduced ,
+     * copied,or used in other ways without permission.Otherwise Jlx will have the right to pursue legal responsibilities.
+     */
+    public class gin
+    {
+      static  byte[] Keys = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+
+      /// <summary>
+      /// 加密
+      /// </summary>
+      /// <param name="text">加密的字符串</param>
+      /// <param name="encryptKey">秘钥</param>
+      /// <returns>返回值</returns>
+      public static string OnKey(string text,string encryptKey)//加密
+      {
+          try
+          {
+              byte[] rgbKey = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+              byte[] rgbIV = Keys;
+              byte[] inputByteArray = Encoding.UTF8.GetBytes(text);
+              DESCryptoServiceProvider dCSP = new DESCryptoServiceProvider();
+              MemoryStream mStream = new MemoryStream();
+              CryptoStream cStream = new CryptoStream(mStream, dCSP.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+              cStream.Write(inputByteArray, 0, inputByteArray.Length);
+              cStream.FlushFinalBlock();
+              return Convert.ToBase64String(mStream.ToArray());
+          }
+          catch
+          {
+              return text;
+          }
+        }
+      /// <summary>
+      /// 解密
+      /// </summary>
+      /// <param name="text">加密后的字符串</param>
+      /// <param name="decryptKey">秘钥</param>
+      /// <returns>返回值</returns>
+      public static string OffKey(string text, string decryptKey) //解密
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey);
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Convert.FromBase64String(text);
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Encoding.UTF8.GetString(mStream.ToArray());
+            }
+            catch
+            {
+                return text;
+            }
+        }
+
+ 
+    }
+    /// <summary>
+    /// 获取IP和地址类
+    /// </summary>
+    public class GetIpAddress
+    {
+        /// <summary>
+        /// 获取IP地址
+        /// </summary>
+        /// <returns></returns>
+        public string GetIP()
+        {
+            string tempip = "";
+            try
+            {
+                WebRequest wr = WebRequest.Create("http://www.ip138.com/ips138.asp");
+                Stream s = wr.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                string all = sr.ReadToEnd(); //读取网站的数据
+                int start = all.IndexOf("您的IP地址是：[") + 9;
+                int end = all.IndexOf("]", start);
+                tempip = all.Substring(start, end - start);
+                sr.Close();
+                s.Close();
+            }
+            catch
+            {
+            }
+            return tempip;
+        }
+        /// <summary>
+        /// 获取对应物理地址
+        /// </summary>
+        /// <returns></returns>
+        public string GetAddress(string add)
+        {
+            string address = "";
+            try
+            {
+                string s1 = "http://www.ip138.com/ips1388.asp?ip=";
+                string s2 = "&action=2";
+                WebRequest wr = WebRequest.Create(s1 + add + s2);
+                Stream s = wr.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                string all = sr.ReadToEnd(); //读取网站的数据
+                int start = all.IndexOf("本站主数据：");
+                int end = all.IndexOf("</li><li>参考数据一：", start);
+                address = all.Substring(start, end - start);
+                address = address.Replace("本站主数据：", "IP信息：");
+                sr.Close();
+                s.Close();
+            }
+            catch
+            {
+            }
+            return address;
+        }
+    }
+    /// <summary>
+    /// 获取时间
+    /// </summary>
+    public class timer
+    {
+        /// <summary>
+        /// 获取北京时间
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetBeiJingTime()
+        {
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://open.baidu.com/special/time/");//百度北京时间地址
+
+            req.Headers.Add("content", "text/html; charset=utf-8");
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+            Stream stream = res.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream, Encoding.GetEncoding("utf-8"));
+
+            string html = sr.ReadToEnd();
+
+            string time = GetRegexStr(html, "(?<=baidu_time\\().*?(?=\\))").Substring(0, 10);//这里是时间戳 不是时间 要转换 
+
+            stream.Dispose();
+
+            sr.Dispose();
+
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+
+            long lTime = long.Parse(time + "0000000");
+
+            TimeSpan toNow = new TimeSpan(lTime);
+
+            return dtStart.Add(toNow);
+
+        }
+
+
+
+        private static string GetRegexStr(string html, string regex)
+        {
+
+            Regex reg = new Regex(regex);
+
+            string result = reg.Matches(html)[0].Value;
+
+            return result;
+
+        }
+    }
+
+/// <summary>
+/// INI文件操作函数
+/// </summary>
+    public class OpenIni
+    { 
+    
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key,
+            string val, string filePath);
+
+        [DllImport("kernel32")]//返回取得字符串缓冲区的长度
+        private static extern long GetPrivateProfileString(string section, string key,
+            string def, StringBuilder retVal, int size, string filePath);
+
+        /// <summary>
+        /// 检查文件是否存在，不存在就创建该配置文件
+        /// </summary>
+        /// <param name="FileName">文件名</param>
+        public void inspect(string FileName)
+        {
+            if (File.Exists(@".\" + FileName))
+            {
+                return;
+            }
+            else
+            {
+                FileInfo fi = new FileInfo(FileName);
+                fi.Create().Close();
+            }
+        }
+
+       #region 读Ini文件
+        /// <summary>
+        /// 读取INI文件内容
+        /// </summary>
+        /// <param name="Section">节点</param>
+        /// <param name="Key">字段</param>
+        /// <param name="NoText">字段值</param>
+        /// <param name="iniFilePath">文件地址</param>
+        /// <returns></returns>
+        public static string ReadIniData(string Section,string Key,string NoText,string iniFilePath)
+        {
+            if(File.Exists(iniFilePath))
+            {
+                StringBuilder temp = new StringBuilder(1024);
+                GetPrivateProfileString(Section,Key,NoText,temp,1024,iniFilePath);
+                return temp.ToString();
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        #endregion
+
+       #region 写Ini文件
+        /// <summary>
+        /// 写INI文件
+        /// </summary>
+        /// <param name="Section">节</param>
+        /// <param name="Key">字段</param>
+        /// <param name="Value">字段值</param>
+        /// <param name="iniFilePath">地址</param>
+        /// <returns></returns>
+        public static bool WriteIniData(string Section,string Key,string Value,string iniFilePath)
+        {
+            if(File.Exists(iniFilePath))
+            {
+                long OpStation = WritePrivateProfileString(Section,Key,Value,iniFilePath);    
+                if(OpStation == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+   
+    }
+
+}
---
+++ jlx/data.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace jlx
+{
+    /*
+    Copyright © 2015，Adam，All Rights Reserved.
+ * Copyright ownership belongs to JLX,shall not be reproduced ,
+ * copied,or used in other ways without permission.Otherwise Jlx will have the right to pursue legal responsibilities.
+ */
+
+    /// <summary>
+    /// 数据库连接
+    /// </summary>
+    public class data
+    {
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
+        /// <param name="source">服务器地址</param>
+        /// <param name="database">数据库名</param>
+        /// <param name="id">用户名</param>
+        /// <param name="pwd">密码</param>
+        /// <returns></returns>
+        
+        string source = jlx.gin.OffKey("G0xv9//YHBznTd9m9LT58A==", "31464333");
+        string bases = jlx.gin.OffKey("L4SYY/H38s0hcrm8C/ulhQ==", "31464333");
+        string Uid = jlx.gin.OffKey("L4SYY/H38s0hcrm8C/ulhQ==", "31464333");
+        string Pwd = jlx.gin.OffKey("VN+ErqHfMxePYD9mRKQA2Q==", "31464333");
+         public bool Login(string id, string pwd)
+         {
+           string sql =string.Format(@"Data Source={0};Database={1};Uid={2};Pwd={3};pooling=true",source,bases,Uid,pwd);
+           SqlConnection conn = new SqlConnection(sql);
+           conn.Open();
+           string sqlstr = "Select Pwd from GmLogin WHERE Uid='" + id + "'";
+           SqlCommand cmd = new SqlCommand(sqlstr, conn);
+           SqlDataReader dr = cmd.ExecuteReader();
+           if (dr["Pwd"].ToString() == pwd)
+           {
+               return true;
+           }
+           else
+               return false;
+           conn.Close();
+         }
+      
+        
+    }
+
+    /// <summary>
+    /// SQL类
+    /// </summary>
+    public static class SqlHelper
+    {
+
+        //把连接字符串写在App.config文件中
+        private static string connStr = @"Data Source=127.0.0.1;Database=Books;Uid=sa;Pwd=a123456;pooling=true";
+        //参数使用可变参数，params，在需要传递参数的时候传递，不需要的时候可以不写
+
+        /// <summary>
+        /// 执行SQL语句 并返回受影响行数
+        /// </summary>
+        /// <param name="sql">SQL语句</param>
+        /// <param name="parameters">参数</param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+
+                    //foreach (SqlParameter  param in parameters)
+                    //{
+                    //    cmd.Parameters.Add(param);
+                    //}
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        /// <summary>
+        /// 查询/返回DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static DataTable ExecuteDataTable(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet dataset = new DataSet();
+                    adapter.Fill(dataset);
+                    return dataset.Tables[0];
+                }
+            }
+        }
+    }
+}
